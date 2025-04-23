db5686c7a9c52ff6b9b69e17b6087d917cc4ade1
Nurmuhammad14456 <nabdujalilov036@gmail.com>
2025-04-23 14:14:56 +0500

    masala

---
+++ js/app.js
@@ -0,0 +1,95 @@
+// {
+// m-1
+//   let k = 7;
+//   let n = 4;
+//   for (let i = 0; i < n; i++) {
+//     console.log(i);
+//   }
+// }
+
+// {
+//   // m -2
+//   let a = 10;
+//   let b = 15;
+//   for (let i = a; i < 1 + b; i++) {
+//     console.log(i);
+//   }
+// }
+
+{
+  // m - 3
+  // let a = 5;
+  // let b = 20;
+  // for (let i = a + 1; i < b; i++) {
+  //   console.log(i);
+  // }
+}
+
+// {
+//   // m - 4;
+//   let narx = 5000;
+//   for (let i = 1; i <= 10; i++) {
+//     let hisob = i * narx;
+//     console.log(hisob);
+//   }
+// }
+
+// {
+//   // m-5
+//   let narxi = 25000;
+//   for (let i = 1; i <= 6; i++) {
+//     let pulim = i / 10;
+//     let ikala = pulim * narxi;
+//     console.log(`${pulim.toFixed(1)} kg = ${ikala}`);
+//   }
+// }
+
+// {
+//   // m-6
+//   let narx = 2000;
+//   for (let ogirlik = 1.2; ogirlik <= 2.0; ogirlik += 0.2) {
+//     let hsob = ogirlik * narx;
+//     console.log(`${ogirlik.toFixed(1)} kg = ${hsob} som`);
+//   }
+// }
+
+{
+  // m-7
+  // let a = 2;
+  // let b = 4;
+  // let sum = 0;
+  // for (let i = a; i <= b; i++) {
+  //   sum += i;
+  // }
+  // console.log(`Yig'indi: ${sum}`);
+}
+
+{
+  // m-8
+  // let a = 3;
+  // let b = 5;
+  // let rest = 1;
+  // for (let i = a; i <= b; i++) {
+  //   rest *= i;
+  //   console.log(`Kopaytmasi: ${rest}`);
+  // }
+}
+{
+  // m-9
+  // let a = 2;
+  // let b = 4;
+  // let sum = 0;
+  // for (let i = a; i <= b; i++) {
+  //   sum += i * i;
+  //   console.log(`kvadrad ${sum}`);
+  // }
+}
+{
+  // m-10
+  let n = 10;
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += 1 / i;
+  }
+  console.log(sum);
+}
